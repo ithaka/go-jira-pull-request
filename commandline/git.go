e5ab5c6addf354c55eaf8289e9d8a51acf4d82df
David Braley <david.braley@gmail.com>
2018-03-01 12:24:08 -0500

    Setup basics test functioantlity sorta

--- commandline/git.go
+++ commandline/git.go
@@ -5,9 +5,16 @@ import "os/exec"
 
 // Man, I go no unit test!
 func IsGitRepo() bool {
-	if _, err := exec.Command("git", "rev-parse", "--is-inside-work-tree").Output() ; err != nil {
+	cmd := exec.Command
+	return IsGitRepoWithExec(cmd)
+}
+
+
+func IsGitRepoWithExec(cmd func(string, ...string) *exec.Cmd ) bool {
+	if _, err := cmd("git", "rev-parse", "--is-inside-work-tree").Output() ; err != nil {
 		return false
 	} else {
 		return true
 	}
+
 }
